bfa0a8ee39a0483e2940c51d1417e946d7d0a7b6
hamabe-kayo <1.15wing.yng@gmail.com>
2022-02-19 15:18:35 +0000

    [Add]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,55 +3,17 @@ class Public::OrdersController < ApplicationController
 
   def new
     @order=Order.new
+    @order.customers_id=current_customer.id
     @shipping_addresses=ShippingAddress.all
   end
 
   def log
     @order = Order.new(order_params)
-    # @shipping_address=ShippingAddress.find(params[:order][:shipping_address_id])
-
-
-    if params[:order][:select_address] == "0"
-      @order.shipping_postal_code=current_customer.postal_code
-      @order.shipping_address=current_customer.address
-      @order.shipping_name=current_customer.full_name
-    elsif params[:order][:select_address] == "1"
-      @order.shipping_postal_code=ShippingAddress.find(params[:order][:shipping_address_id]).postal_code
-      @order.shipping_address=ShippingAddress.find(params[:order][:shipping_address_id]).address
-      @order.shipping_name=ShippingAddress.find(params[:order][:shipping_address_id]).name
-    else
-      @order.shipping_postal_code=order_params[:shipping_postal_code]
-      @order.shipping_address=order_params[:shipping_address]
-      @order.shipping_name=order_params[:shipping_name]
-    end
-
-    @cart_items=current_customer.cart_items.all
+    @order.customers_id=current_customer.id
+    @cart_items=CartItem.all
     @total=@cart_items.inject(0) { |sum, item| sum + item.subtotal }
-  end
+    @payment=(@total + 800).to_s
 
-  def thanks
-  end
-
-  def create
-    @order=Order.new(order_params)
-    @cart_items=current_customer.cart_items.all
-
-    if @order.save
-      @cart_items.each do |cart_item|
-        order_detail=OrderDetail.new
-        order_detail.item_id=cart_item.item_id
-        order_detail.order_id=@order.id
-        order_detail.order_amount=cart_item.amount
-        order_detail.order_price=cart_item.sutotal
-        order_detail.save
-      end
-      redirect_to thanks_orders_path
-      cart_items.destroy_all
-    else
-      @order=Order.new(order_params)
-      render :new
-    end
-    
     if params[:order][:select_address] == "0"
       @order.shipping_postal_code=current_customer.postal_code
       @order.shipping_address=current_customer.address
@@ -64,15 +26,39 @@ class Public::OrdersController < ApplicationController
       @order.shipping_postal_code=order_params[:shipping_postal_code]
       @order.shipping_address=order_params[:shipping_address]
       @order.shipping_name=order_params[:shipping_name]
-    else
-      render :new
     end
-    redirect_to thanks_orders_path
 
+  end
+
+  def thanks
+  end
+
+  def create
     # binding.pry
+
+    @order=Order.new(order_params)
+    @order.customers_id=current_customer.id
+    @cart_items=current_customer.cart_items.all
+
+    if @order.save
+      @cart_items.each do |cart_item|
+        @order_detail=OrderDetail.new
+        @order_detail.order_id=@order.id
+        @order_detail.item_id=cart_item.item.id
+        @order_detail.order_price=cart_item.item.with_tax_price
+        @order_detail.amount=cart_item.amount
+        @order_detail.making_status=0
+        @order_detail.save
+      end
+      @cart_items.destroy_all
+      redirect_to thanks_orders_path
+    else
+      redirect_to new_order_path
+    end
   end
 
   def index
+    @orders=OrderDetail.all
   end
 
   def show
@@ -80,7 +66,7 @@ class Public::OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order).permit(:customers_id, :shipping_postal_code, :shipping_address, :shipping_name, :payment_method)
+    params.require(:order).permit(:customers_id, :shipping_postal_code, :shipping_address, :shipping_name, :payment_method, :payment)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
-  belongs_to :customer
+  belongs_to :customer, optional: true
 
   enum payment_method: {credit_card: 0, transfer: 1}
   enum order_status: {not_payment: 0, payment_confirmation: 1, under_production: 2, preparing_to_ship: 3, sent: 4}
